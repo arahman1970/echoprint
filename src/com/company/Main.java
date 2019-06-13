f59cc8caa41127100d83c3df256f6a869a574654
arahman1970 <ar.nj.usa@gmail.com>
2019-06-13 14:27:44 -0400

    echo print

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Cost" + "\t\tQuantity" + "\t\tTotal");
+        System.out.print("$1,000.00 " +"\t\t4" +"\t\t$4,000.00");
+        System.out.print("\n$\t50.00" + " " + "\t\t8" + "\t\t$" + " " + " 400.00");
+        System.out.print("\n_________________________________");
+        System.out.println("\nTotal" + "\t\t\t12" + "\t\t$4,400.00");
+    }
+    }
+
